7503b599084bddb420884443c6b4260f753d1bd7
Mats Rauhala <mats.rauhala@iki.fi>
2021-04-29 15:22:57 +0300

    Dhall lsp
    Change-Id: Ib6ea5a848e751a9c32a5b027108ca7aef41024af

--- default.el/dhall.el
+++ default.el/dhall.el
@@ -1,1 +1,14 @@
-(use-package dhall-mode)
+(defun setup-dhall-lsp ()
+  (add-to-list 'lsp-language-id-configuration '(dhall-mode . "dhall"))
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection (lsp-stdio-connection "dhall-lsp-server")
+    :activation-fn (lsp-activate-on "dhall")
+    :server-id 'dhall-lsp-server
+    ))
+  )
+(use-package dhall-mode
+  :after lsp-mode
+  :config
+  (setup-dhall-lsp)
+  )
