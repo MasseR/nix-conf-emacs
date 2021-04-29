e5a70989bb7b07bf758d44d5b6a5f697fd5563c3
Mats Rauhala <mats.rauhala@iki.fi>
2021-04-29 11:49:45 +0300

    Start the LSP automatically on any .envrc enabled dir
    Change-Id: I5e8d9b7b0e7af40f2c13d387e695c266b1b55296

--- default.el/lsp.el
+++ default.el/lsp.el
@@ -1,10 +1,24 @@
 (use-package lsp-mode)
 (use-package lsp-ui)
 
+; https://github.com/wbolster/emacs-direnv/issues/28#issuecomment-500388468
+(defun prepare-haskell-in-shell (&rest args)
+  (setq tried-to-start-lsp t)
+  ;; (when (eq major-mode 'haskell-mode)
+  ;;   (setq tried-to-start-lsp t)
+  ;;   (lsp))
+  )
+
 ;; Haskell configuration
+;; https://github.com/wbolster/emacs-direnv/issues/28
+(define-advice direnv-update-directory-environment (:after (x) haskell-after-direnv)
+  (when (eq major-mode 'haskell-mode)
+	 (lsp)))
 (use-package lsp-haskell
-	     :after lsp-mode
-	     :config
-	     (add-hook 'haskell-mode-hook #'lsp)
-	     (add-hook 'haskell-literate-mode-hook #'lsp)
-	     )
+  :after lsp-mode direnv-mode
+  :config
+  (add-hook 'haskell-mode-hook #'lsp)
+  (add-hook 'haskell-literate-mode-hook #'lsp)
+  
+
+  )
