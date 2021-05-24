560eaefb0f72bd31a5cf57f6f71728c2114b825c
Mats Rauhala <mats.rauhala@iki.fi>
2021-05-24 09:42:44 +0300

    Smartparens for dhall
    Change-Id: Ieb156348495f0f44c8b4da22ee9589735e73b5b6

--- default.el/dhall.el
+++ default.el/dhall.el
@@ -10,6 +10,7 @@
 (use-package dhall-mode
   :after eglot
   :config
+  (sp-local-pair 'dhall-mode "\\(" ")")
   (add-to-list 'eglot-server-programs '(dhall-mode . ("dhall-lsp-server")))
   (evil-leader/set-key-for-mode 'dhall-mode
     ; No code actions for dhall :(
