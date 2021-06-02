8557729603d95f1d6e1f5d4633f5568721a63639
Mats Rauhala <mats.rauhala@iki.fi>
2021-06-02 15:40:23 +0300

    Remove dead code
    Change-Id: I904b1a91abab8b3f90ca3ebfd959512f3e4dda38

--- default.el/dhall.el
+++ default.el/dhall.el
@@ -1,12 +1,3 @@
-(defun setup-dhall-lsp ()
-  (add-to-list 'lsp-language-id-configuration '(dhall-mode . "dhall"))
-  (lsp-register-client
-   (make-lsp-client
-    :new-connection (lsp-stdio-connection "dhall-lsp-server")
-    :activation-fn (lsp-activate-on "dhall")
-    :server-id 'dhall-lsp-server
-    ))
-  )
 (use-package dhall-mode
   :after eglot
   :config
