1dfae0826f5cb13ffb84feb8bb899a0bb3449da8
Mats Rauhala <mats.rauhala@iki.fi>
2021-06-09 15:46:54 +0300

    The lines whitepsace thing is annoying
    Change-Id: I2e2753528aeaa0d0f8310aa378d6e5167507c995

--- default.el/whitespace.el
+++ default.el/whitespace.el
@@ -2,7 +2,7 @@
 (global-whitespace-mode 1)
 (setq
  ; Show trailing whitespace, tabs, and too long line
- whitespace-style '(face trailing tabs tab-mark lines)
+ whitespace-style '(face trailing tabs tab-mark)
  ; The too long line is defined to 100 columns
  whitespace-lines-column 100
  )
