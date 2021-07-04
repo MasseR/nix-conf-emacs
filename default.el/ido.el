8050da351440174e0b1c6a0562d8bbb92cc4931f
Mats Rauhala <mats.rauhala@iki.fi>
2021-07-04 18:18:43 +0300

    Reindent
    Change-Id: I4c55f54cd8ec91b4499eef60f1edbf28796af168

--- default.el/ido.el
+++ default.el/ido.el
@@ -1,14 +1,14 @@
 (use-package ido
-	     :config
-	     (ido-mode 1)
-	     (ido-everywhere 1)
-	     (ido-vertical-mode 1)
-	     (setq ido-enable-flex-matching t
-		   ido-everywhere t
-		   ido-vertical-define-keys 'C-n-C-p-up-and-down))
+  :config
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (ido-vertical-mode 1)
+  (setq ido-enable-flex-matching t
+        ido-everywhere t
+        ido-vertical-define-keys 'C-n-C-p-up-and-down))
 
 (use-package flx-ido
   :config
   (flx-ido-mode 1)
   (setq ido-enable-flex-matching t
-	ido-use-faces nil))
+        ido-use-faces nil))
