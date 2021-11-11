3c6be99f155f963f8e16f52b6671f0416549579a
Mats Rauhala <mats.rauhala@iki.fi>
2021-11-11 16:50:02 +0200

    Update emacs
    Change-Id: I7ec7c16d99fa755a20bb26f748f9c4301e42905d

--- default.el/org.el
+++ default.el/org.el
@@ -1,6 +1,31 @@
+;; Reminders taken from
+;; http://doc.norang.ca/org-mode.html
+(defun bh/org-agenda-to-appt ()
+  (interactive)
+  ; Clear the appt list
+  (setq appt-time-msg-list nil)
+  ; Recreate it from org-agenda
+  (org-agenda-to-appt))
+
+;; appt notifications through notifications
+;; https://emacs.stackexchange.com/questions/3844/good-methods-for-setting-up-alarms-audio-visual-triggered-by-org-mode-events
+;; Use both the notifications system and the original value
+(defun appt-notify (min-to-app new-time appt-msg)
+  (notifications-notify :title "Appointment"
+                        ; Doc says the time is a string
+                        :body (format "Appointment in %s minutes: %s" min-to-app appt-msg)
+                        :app-name "Emacs: Org"
+                        :timeout 0
+                        ; I don't seem to have this audio
+                        :sound-name "alarm-clock-elapsed")
+  (appt-disp-window min-to-app new-time appt-msg))
+
+
 (use-package org
   :after evil evil-leader
   :config
+  ; Add a hook for whenever the agenda is finalized to create the appointments
+  (add-hook 'org-agenda-finalize-hook 'bh/org-agenda-to-appt 'append)
   (setq
    ;; Set this manually in each environments .emacs
    ;; org-agenda-files '("~/orgmode/" "~/personal_orgmode")
@@ -32,3 +57,12 @@
     )
   )
 (use-package org-protocol)
+
+(use-package appt
+  :config
+  (setq
+   appt-message-warning-time 15
+   appt-display-mode-line t
+   appt-display-format 'window
+   appt-disp-window-function 'appt-notify)
+  (appt-activate t))
