1d7837109f66c2ccabff8f38be72c17f95cbf55d
Mats Rauhala <mats.rauhala@iki.fi>
2023-03-03 14:17:13 +0200

    Removed the old overrides

--- default.nix
+++ default.nix
@@ -2,39 +2,6 @@
 
 let
   default-el = callPackage ./default.el {};
-  # I'm leaving the overrides here as a guide to the future
-  overrides = self: super: rec {
-    project = self.callPackage ({ elpaBuild, emacs, fetchurl, lib, xref }:
-      elpaBuild {
-        pname = "project";
-        ename = "project";
-        version = "0.6.0";
-        src = fetchurl {
-          url = "https://elpa.gnu.org/packages/project-0.6.0.tar";
-          sha256 = "0m0r1xgz1ffx6mi2gjz1dkgrn89sh4y5ysi0gj6p1w05bf8p0lc0";
-        };
-        packageRequires = [ emacs xref ];
-        meta = {
-          homepage = "https://elpa.gnu.org/packages/project.html";
-          license = lib.licenses.free;
-        };
-      }) {};
-    xref = self.callPackage ({ elpaBuild, emacs, fetchurl, lib }:
-      elpaBuild {
-        pname = "xref";
-        ename = "xref";
-        version = "1.1.0";
-        src = fetchurl {
-          url = "https://elpa.gnu.org/packages/xref-1.1.0.tar";
-          sha256 = "1s7pwk09bry4nqr4bc78a3mbwyrxagai2gpsd49x47czy2x7m3ax";
-        };
-        packageRequires = [ emacs ];
-        meta = {
-          homepage = "https://elpa.gnu.org/packages/xref.html";
-          license = lib.licenses.free;
-        };
-      }) {};
-  };
 
 in
 
