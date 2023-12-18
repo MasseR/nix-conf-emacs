f282c71ac42ac6bc69e324fe40aef31da5aad75d
Mats Rauhala <mats.rauhala@iki.fi>
2023-12-18 11:12:45 +0200

    Update the org-roam templates

--- default.el/org-roam.el
+++ default.el/org-roam.el
@@ -14,19 +14,11 @@
                                  :unnarrowed t)
                                 ("m" "meeting" plain "%?"
                                  :target (file+head  "%<%Y%m%d%H%M%S>-${slug}.org"
-                                                     "#+title: ${title}\n\n* Project\n* Attendees\n* Agenda\n* Goals\n* Discussion notes\n* Action items\n")
-                                 :unnarrowed t)
-                                ("c" "Contact" plain "%?"
-                                 :target (file+head  "%<%Y%m%d%H%M%S>-${slug}.org"
-                                                     "#+title: ${title}\nTeam:")
+                                                     "#+title: %<%Y-%m-%d> ${title}\n\n* Project\n* Attendees\n* Agenda\n* Goals\n* Discussion notes\n* Action items\n")
                                  :unnarrowed t)
                                 ("p" "Project" plain "%?"
                                  :target (file+head  "%<%Y%m%d%H%M%S>-${slug}.org"
-                                                     "#+title: ${title}\n\n* Project description\n* Project information\n\n** Design\n** Epic\n* Resources\n\n** Shepherd\n** Architect")
-                                 :unnarrowed t)
-                                ("i" "Issue" plain "%?"
-                                 :target (file+head  "%<%Y%m%d%H%M%S>-${slug}.org"
-                                                     "#+title: ${title}\nAuthor::.\nProject::.\n")
+                                                     "#+title: %<%Y-%m-%d> ${title}\n\n* Project description\n* Project information\n\n")
                                  :unnarrowed t)
                                 )
    )
