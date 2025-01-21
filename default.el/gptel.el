bd868e6013f239f57d79e595ad0bf9bafe00f42a
Mats Rauhala <mats.rauhala@iki.fi>
2025-01-21 14:27:46 +0200

    Use ollama by default, but openrouter works as well

--- default.el/gptel.el
+++ default.el/gptel.el
@@ -1,6 +1,16 @@
 (use-package gptel
   :config
-  (gptel-make-ollama "Ollama"
+  (setq gptel-backend (gptel-make-ollama "Ollama"
                        :host "localhost:11434"
                        :stream t
-                       :models '(llama3.2:latest)))
+                        :models '(llama3.2:latest)))
+  (gptel-make-openai "OpenRouter"
+    :host "openrouter.ai"
+    :endpoint "/api/v1/chat/completions"
+    :stream t
+    ; For some reason the automatic detection didn't work
+    ; The authentication info is stored in one of ~/.authinfo or ~/.authinfo.gpg
+    :key (auth-source-pick-first-password :host "openrouter.ai")
+    :models '(openai/gpt-4o-mini
+               meta-llama/codellama-34b-instruct
+               anthrophic/claude-3-5-sonnet)))
