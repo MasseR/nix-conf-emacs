39d9feafd2a588c9f9bd0e3977a30468de1f6ed5
Mats Rauhala <mats.rauhala@iki.fi>
2025-01-21 10:48:47 +0200

    Try to make ledger mode work with hledger

--- default.el/ledger.el
+++ default.el/ledger.el
@@ -5,5 +5,8 @@
    ledger-binary-path "hledger"
    ledger-report-links-in-register nil
    ledger-mode-should-check-version nil
+   ledger-init-file-name " "
+   ledger-highlight-xact-under-point nil
    )
+  (add-hook 'ledger-mode-hook 'auto-revert-tail-mode)
   )
