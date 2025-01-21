e581876497922ae7ab5cb95dcbd3d7d50d951a2e
Mats Rauhala <mats.rauhala@iki.fi>
2025-01-21 12:59:05 +0200

    Reindent

--- default.el/neotree.el
+++ default.el/neotree.el
@@ -1,16 +1,16 @@
 (use-package neotree
-	     :after evil evil-leader
-	     :config
-	     (evil-leader/set-key
-	       "ft" 'neotree-toggle)
-	     (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
-	     (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
-	     (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
-	     (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
-	     (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
-	     (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
-	     (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
-	     (evil-define-key 'normal neotree-mode-map (kbd "ma") 'neotree-create-node)
-	     (evil-define-key 'normal neotree-mode-map (kbd "md") 'neotree-delete-node)
-	     (evil-define-key 'normal neotree-mode-map (kbd "mm") 'neotree-rename-node)
-	     )
+  :after evil evil-leader
+  :config
+  (evil-leader/set-key
+    "ft" 'neotree-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+  (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+  (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "ma") 'neotree-create-node)
+  (evil-define-key 'normal neotree-mode-map (kbd "md") 'neotree-delete-node)
+  (evil-define-key 'normal neotree-mode-map (kbd "mm") 'neotree-rename-node)
+  )
